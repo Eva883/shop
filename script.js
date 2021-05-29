4d9dee6d9f3808b9097adff12b9a0659386c0aae
Eva883 <80159739+Eva883@users.noreply.github.com>
2021-05-29 16:06:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-var card = document.querySelector('.card')
-var product_field = document.querySelector('.product_field')
-console.log(card.outerHTML)
-var card_html = '';
+window.onload = function(){
+    $.getJSON("https://spreadsheets.google.com/feeds/list/10GiBKjBcXYvONUEJJtxzn1XNmyqph24Qn7phqOZK5dg/od6/public/values?alt=json",
+    function(data){
+        data = data['feed']['entry']
+        console.log(data);
+        show(data);
+    }
+    )
+    function show(data){
+        var out = '';
+        for(var i = 0; i < data.length; i++){
 
-for( var i = 0; i < 5; i++)
-{
-    card_html += card.outerHTML;
-    product_field.innerHTML = card_html;
+            out +=`<div class="card">`;
+            out +=`<h3 class="name_card">${data[i]['gsx$name']['$t']}</h3>`;
+            out +=`<img src="${data[i]['gsx$image']['$t']}" alt="" class="img_card">`;
+            out +=`<p class="price_card">${data[i]['gsx$price']['$t']}</p>`;
+            out +=`</div>`;
+
+        }
+        document.querySelector('.product_field').innerHTML = out;
+    }
 }
